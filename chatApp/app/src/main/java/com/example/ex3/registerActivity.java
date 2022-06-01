3e3fb15e4fe38d7bee2b70e5b53f65c54b745831
raghebghazi <raghep-11@hotmail.com>
2022-06-01 22:24:10 +0300

    add : upload profile picture button

--- chatApp/app/src/main/java/com/example/ex3/registerActivity.java
+++ chatApp/app/src/main/java/com/example/ex3/registerActivity.java
@@ -1,18 +1,33 @@
 package com.example.ex3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class registerActivity extends AppCompatActivity {
+    int SELECT_PHOTO = 1;
+    Uri uri;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //sign in button
         TextView btn = findViewById(R.id.signinlink);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +36,34 @@ public class registerActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //add image button
+        Button choose = findViewById(R.id.AddImageBtn);
+        imageView = findViewById(R.id.image);
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,SELECT_PHOTO);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==SELECT_PHOTO && resultCode ==RESULT_OK && data != null && data.getData() != null){
+            uri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
